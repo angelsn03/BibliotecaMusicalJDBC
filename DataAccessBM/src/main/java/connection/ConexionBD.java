80499cc9c0c4bdbd9e8b035319bf084db6b3eb5e
angelsn <114720318+angelsn03@users.noreply.github.com>
2024-12-12 23:27:36 -0700

    Conexión con la bd

---
+++ DataAccessBM/src/main/java/connection/ConexionBD.java
@@ -0,0 +1,41 @@
+
+package connection;
+
+import interfaces.ConexionBDImp;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+
+/**
+ *
+ * @author angelsn
+ */
+public class ConexionBD implements ConexionBDImp{
+    
+    public static ConexionBD conexion;
+    private Connection connection;
+    private String url = "jdbc:mysql://localhost:3306/biblioteca2";
+    private String user = "root";
+    private String password = "admin";
+    
+    private ConexionBD(){
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", ERROR_MESSAGE);
+        }
+    }
+    
+    public static ConexionBD getInstance(){
+        if(conexion == null){
+            conexion = new ConexionBD();
+        }
+        return conexion;
+    }
+ 
+    @Override
+    public Connection getConnection(){
+        return connection;
+    }
+}
